5c6ca2aa99e92c398e9e2267851a2a6eaf6353f3
rezanejati <dev.njti@gmail.com>
2018-11-24 09:57:15 +0330

    init

---
+++ app/src/main/java/library/android/eniac/myapplication/MainActivity2.java
@@ -0,0 +1,9 @@
+package library.android.eniac.myapplication;
+
+/**
+ * Created by RezaNejati on 11/23/2018.
+ */
+public class MainActivity2 {
+
+
+}
---
+++ app/src/main/java/library/android/eniac/myapplication/Test.java
@@ -0,0 +1,7 @@
+package library.android.eniac.myapplication;
+
+/**
+ * Created by RezaNejati on 11/23/2018.
+ */
+public class Test {
+}
